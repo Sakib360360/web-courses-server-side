8c001c6ad39b2893d3ae90c04b2416cb5492afa6
Sakib360360 <saakibabrar@gmail.com>
2023-10-18 22:39:58 +0600

    no

--- index.js
+++ index.js
@@ -182,6 +182,12 @@ async function run() {
     });
 
 
+
+   
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
